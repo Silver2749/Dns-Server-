2611668a56777b088ba641e011d73782d3c8b5df
Silver2749 <7shanebraganza@gmail.com>
2026-01-19 14:30:14 +0530

    first commit

---
+++ target/debug/deps/dns_server.d
@@ -0,0 +1,5 @@
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\deps\dns_server.d: src\main.rs
+
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\deps\dns_server.exe: src\main.rs
+
+src\main.rs:
---
+++ target/debug/dns-server.d
@@ -0,0 +1,1 @@
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\dns-server.exe: C:\Users\91859\Dev-Shi\rust\dns-server\src\main.rs
