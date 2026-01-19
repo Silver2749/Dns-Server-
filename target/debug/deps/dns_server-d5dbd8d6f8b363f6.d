c500905f8babca5554921005dc280e250c14012f
Silver2749 <7shanebraganza@gmail.com>
2026-01-19 17:06:31 +0530

    second commit

---
+++ target/debug/deps/dns_server-d5dbd8d6f8b363f6.d
@@ -0,0 +1,5 @@
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\deps\dns_server-d5dbd8d6f8b363f6.d: src\main.rs
+
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\deps\libdns_server-d5dbd8d6f8b363f6.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/dns_server-dcd025bd99ca53bb.d
@@ -0,0 +1,5 @@
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\deps\dns_server-dcd025bd99ca53bb.d: src\main.rs
+
+C:\Users\91859\Dev-Shi\rust\dns-server\target\debug\deps\libdns_server-dcd025bd99ca53bb.rmeta: src\main.rs
+
+src\main.rs:
